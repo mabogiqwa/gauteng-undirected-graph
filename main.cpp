0f07b321e9ba226554de7ffc86c5c96c368b12b6
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-03-08 21:01:54 +0200

    Added lines of code in main

--- main.cpp
+++ main.cpp
@@ -1,3 +1,35 @@
+//Source code for the graph that will contain 
+//data related to the distances between locations in
+//Gauteng map
 #include <iostream>
 
-int main
+struct Node
+{
+    double distance;
+    Node *link;
+};
+typedef Node* NodePtr;
+
+void add_node_to_list(NodePtr &currentNode, double distance);
+
+int main()
+{
+    NodePtr list = new Node[32];
+
+    add_node_to_list(list[0], distance)
+
+    delete[] list;
+
+    return 0;
+}
+
+void add_node_to_list(NodePtr &currentNode, double distance)
+{
+    //This creates a FIFO data structure
+    NodePtr tempPtr = new Node;
+    tempPtr->coefficient = c;
+    tempPtr->exponent = exp;
+    currentNode->link = tempPtr;
+    currentNode = tempPtr;
+    tempPtr->link = nullptr;
+}
