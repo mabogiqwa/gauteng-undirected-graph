dc79ca8f88b3359eb03e5a9a34b9ab4c51faf433
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-03-09 07:38:48 +0200

    Fixed run time error

--- main.cpp
+++ main.cpp
@@ -12,14 +12,22 @@ struct Node
 typedef Node* NodePtr;
 
 void add_node_to_list(NodePtr &currentNode, double distance);
-//Postcondition: Adds a new node to a FIFO data structure 
+//Postcondition: Adds a new node to a FIFO data structure
 int main()
 {
     Node* listArray[SIZE] = {nullptr};
 
-    add_node_to_list(listArray[0], 22);
-    add_node_to_list(listArray[0], 6.6);
-    add_node_to_list(listArray[0], 36.2);
+    listArray[0] = new Node{0, nullptr};
+    NodePtr head1 = listArray[0];
+
+    add_node_to_list(head1, 22);
+    add_node_to_list(head1, 6.6);
+    add_node_to_list(head1, 36.2);
+
+    for (NodePtr tempPtr = listArray[0]->link; tempPtr != nullptr; tempPtr = tempPtr->link)
+    {
+        std::cout << tempPtr->distance << " ";
+    }
 
     return 0;
 }
