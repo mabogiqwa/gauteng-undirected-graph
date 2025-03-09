ac41c3431f6c950521a682dcea3e9f286dbb8a3d
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-03-09 17:43:52 +0200

    Added destinations

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ struct Node
 };
 typedef Node* NodePtr;
 
-void add_node_to_list(NodePtr &currentNode, double distance);
+void add_node_to_list(NodePtr &currentNode, int destination, double distance);
 //Postcondition: Adds a new node to a FIFO data structure
 
 void create_gauteng_graph(Node** listArray);
@@ -52,7 +52,7 @@ void print_data(Node** listArray)
     }
 }
 
-void add_node_to_list(NodePtr &currentNode, int destination double distance)
+void add_node_to_list(NodePtr &currentNode, int destination, double distance)
 {
     //This creates a FIFO data structure
     NodePtr tempPtr = new Node;
@@ -76,268 +76,268 @@ void create_gauteng_graph(Node** listArray)
     listArray[1] = new Node{0, 22, nullptr}; //(1,0) Sosha->Temba
     NodePtr head2 = listArray[1];
 
-    add_node_to_list(head2, 30.2); //(1,2)
-    add_node_to_list(head2, 10.4); //(1,3)
-    add_node_to_list(head2, 14); //(1,4)
-    add_node_to_list(head2, 51.3); //(1,5)
+    add_node_to_list(head2, 2, 20.2); //(1,2) Sosha->Hammanskraal
+    add_node_to_list(head2, 3, 10.4); //(1,3) Sosha->Winterveld
+    add_node_to_list(head2, 4, 14); //(1,4) Sosha->Klipgat
+    add_node_to_list(head2, 5, 51.3); //(1,5) Sosha->Roodeplaat
 
     //Hammanskraal
-    listArray[2] = new Node{6.6, nullptr}; //(2,0)
+    listArray[2] = new Node{0, 6.6, nullptr}; //(2,0) Hammanskraal->Temba
     NodePtr head3 = listArray[2];
 
-    add_node_to_list(head3, 20.2); //(2,1)
-    add_node_to_list(head3, 43); //(2,5)
-    add_node_to_list(head3, 49.1); //(2,6)
+    add_node_to_list(head3, 1, 20.2); //(2,1) Hammanskraal->Temba
+    add_node_to_list(head3, 5, 43); //(2,5) Hammanskraal->Roodeplaat
+    add_node_to_list(head3, 6, 49.1); //(2,6) Hammanskraal->Refilwe
 
     //Winterveld
-    listArray[3] = new Node{36.2, nullptr}; //(3,0)
+    listArray[3] = new Node{0, 36.2, nullptr}; //(3,0) Winterveld->Temba
     NodePtr head4 = listArray[3];
 
-    add_node_to_list(head4, 10.4); //(3,1)
-    add_node_to_list(head4, 11.8); //(3,4)
+    add_node_to_list(head4, 1, 10.4); //(3,1) Winterveld->Sosha
+    add_node_to_list(head4, 4, 11.8); //(3,4) Winterveld->Klipgat
 
     //Klipgat
-    listArray[4] = new Node{14, nullptr}; //(4,1)
+    listArray[4] = new Node{1, 14, nullptr}; //(4,1) Klipgat->Sosha
     NodePtr head5 = listArray[4];
 
-    add_node_to_list(head5, 11.8); //(4,3)
-    add_node_to_list(head5, 56.3); //(4,5)
-    add_node_to_list(head5, 7.8); //(4,7)
+    add_node_to_list(head5, 3, 11.8); //(4,3) Klipgat->Winterveld
+    add_node_to_list(head5, 5, 56.3); //(4,5) Klipgat->Roodeplaat
+    add_node_to_list(head5, 7, 7.8); //(4,7) Klipgat->Mabopane
 
     //Roodeplaat
-    listArray[5] = new Node{51.3, nullptr}; //(5,1)
+    listArray[5] = new Node{1, 51.3, nullptr}; //(5,1)
     NodePtr head6 = listArray[5];
 
-    add_node_to_list(head6, 43); //(5,2)
-    add_node_to_list(head6, 56.3); //(5,4)
-    add_node_to_list(head6, 27.8); //(5,6)
-    add_node_to_list(head6, 49.7); //(5,7)
-    add_node_to_list(head6, 49.7); //(5,8)
-    add_node_to_list(head6, 60.1); //(5,9)
-    add_node_to_list(head6, 21.6); //(5,10)
+    add_node_to_list(head6, 2, 43); //(5,2) Roodeplaat->Hammanskraal
+    add_node_to_list(head6, 4, 56.3); //(5,4) Roodeplaat->Klipgat
+    add_node_to_list(head6, 6, 27.8); //(5,6) Roodeplaat->Refilwe
+    add_node_to_list(head6, 7, 49.7); //(5,7) Roodeplaat->Mabopane
+    add_node_to_list(head6, 8, 49.7); //(5,8) Roodeplaat->Ga-Rankuwa
+    add_node_to_list(head6, 9, 60.1); //(5,9) Roodeplaat->Hartbeespoort
+    add_node_to_list(head6, 10, 21.6); //(5,10) Roodeplaat->Pretoria
 
     //Refilwe
-    listArray[6] = new Node{49.1, nullptr}; //(6,2)
+    listArray[6] = new Node{2, 49.1, nullptr}; //(6,2) Refilwe->Hammanskraal
     NodePtr head7 = listArray[6];
 
-    add_node_to_list(head7, 27.8); //(6,5)
-    add_node_to_list(head7, 5.5); //(6,11)
-    add_node_to_list(head7, 29.9); //(6,13)
+    add_node_to_list(head7, 5, 27.8); //(6,5) Refilwe->Roodeplaat
+    add_node_to_list(head7, 11, 5.5); //(6,11) Refilwe->Cullinan
+    add_node_to_list(head7, 13, 29.9); //(6,13) Refilwe->Ekangala
 
     //Mabopane
-    listArray[7]  = new Node{7.8, nullptr}; //(7,4)
+    listArray[7]  = new Node{4, 7.8, nullptr}; //(7,4)
     NodePtr head8 = listArray[7];
 
-    add_node_to_list(head8, 49.7); //(7,5)
-    add_node_to_list(head8, 9.9); //(7,8)
+    add_node_to_list(head8, 5, 49.7); //(7,5)
+    add_node_to_list(head8, 8, 9.9); //(7,8)
 
     //Ga-Rankuwa
-    listArray[8] = new Node{49.7, nullptr}; //(8,5)
+    listArray[8] = new Node{5, 49.7, nullptr}; //(8,5)
     NodePtr head9 = listArray[8];
 
-    add_node_to_list(head9, 9.9); //(8,7)
-    add_node_to_list(head9, 24.2); //(8,9)
+    add_node_to_list(head9, 7, 9.9); //(8,7)
+    add_node_to_list(head9, 9, 24.2); //(8,9)
 
     //Hartbeespoort
-    listArray[9] = new Node{60.1, nullptr}; //(9,4)
+    listArray[9] = new Node{4, 60.1, nullptr}; //(9,4)
     NodePtr head10 = listArray[9];
 
-    add_node_to_list(head10, 24.2); //(9,8)
-    add_node_to_list(head10, 33.9); //(9,10)
-    add_node_to_list(head10, 34); //(9,13)
-    add_node_to_list(head10, 82.6); //(9,14)
+    add_node_to_list(head10, 8, 24.2); //(9,8)
+    add_node_to_list(head10, 10, 33.9); //(9,10)
+    add_node_to_list(head10, 13, 34); //(9,13)
+    add_node_to_list(head10, 14, 82.6); //(9,14)
 
     //Pretoria
-    listArray[10] = new Node{21.6, nullptr}; //(10,5)
+    listArray[10] = new Node{5, 21.6, nullptr}; //(10,5)
     NodePtr head11 = listArray[10];
 
-    add_node_to_list(head11, 33.9); //(10,9)
-    add_node_to_list(head11, 41.9); //(10,12)
-    add_node_to_list(head11, 15.1); //(10,13)
+    add_node_to_list(head11, 9, 33.9); //(10,9)
+    add_node_to_list(head11, 12, 41.9); //(10,12)
+    add_node_to_list(head11, 13, 15.1); //(10,13)
 
     //Cullinan
-    listArray[11] = new Node{5.5, nullptr};//(11,8)
+    listArray[11] = new Node{8, 5.5, nullptr};//(11,8)
     NodePtr head12 = listArray[11];
 
-    add_node_to_list(head12, 9.1); //(11,12)
+    add_node_to_list(head12, 12, 9.1); //(11,12)
 
     //Rayton
-    listArray[12] = new Node{41.9, nullptr}; //(12,10)
+    listArray[12] = new Node{10, 41.9, nullptr}; //(12,10)
     NodePtr head13 = listArray[12];
 
-    add_node_to_list(head13, 9.1); //(12,11)
-    add_node_to_list(head13, 33.5); //(12,13)
-    add_node_to_list(head13, 60.2); //(12,14)
-    add_node_to_list(head13, 31.4); //(12,15)
-    add_node_to_list(head13, 57.7); //(12,17)
-    add_node_to_list(head13, 60); //(12,18)
-    add_node_to_list(head13, 53.9); //(12,22)
-    add_node_to_list(head13, 69.2); //(12,23)
+    add_node_to_list(head13, 11, 9.1); //(12,11)
+    add_node_to_list(head13, 13, 33.5); //(12,13)
+    add_node_to_list(head13, 14, 60.2); //(12,14)
+    add_node_to_list(head13, 15, 31.4); //(12,15)
+    add_node_to_list(head13, 17, 57.7); //(12,17)
+    add_node_to_list(head13, 18, 60); //(12,18)
+    add_node_to_list(head13, 22, 53.9); //(12,22)
+    add_node_to_list(head13, 23, 69.2); //(12,23)
 
     //Ekangala
-    listArray[13] = new Node{29.9, nullptr}; //(13,6)
+    listArray[13] = new Node{6, 29.9, nullptr}; //(13,6)
     NodePtr head14 = listArray[13];
 
-    add_node_to_list(head14, 33.5); //(13,12)
-    add_node_to_list(head14, 19.6); //(13,15)
+    add_node_to_list(head14, 12, 33.5); //(13,12)
+    add_node_to_list(head14, 15, 19.6); //(13,15)
 
     //Centurion
-    listArray[14] = new Node{34, nullptr}; //(14,9)
+    listArray[14] = new Node{9, 34, nullptr}; //(14,9)
     NodePtr head15 = listArray[14];
 
-    add_node_to_list(head15, 15.1); //(14,10)
-    add_node_to_list(head15, 60.2); //(14,12)
-    add_node_to_list(head15, 26.2); //(14,16)
-    add_node_to_list(head15, 40.5); //(14,17)
-    add_node_to_list(head15, 52.5); //(14, 19)
-    add_node_to_list(head15, 48.3); //(14, 21)
+    add_node_to_list(head15, 10, 15.1); //(14,10)
+    add_node_to_list(head15, 12, 60.2); //(14,12)
+    add_node_to_list(head15, 16, 26.2); //(14,16)
+    add_node_to_list(head15, 17, 40.5); //(14,17)
+    add_node_to_list(head15, 19, 52.5); //(14, 19)
+    add_node_to_list(head15, 21, 48.3); //(14, 21)
 
     //Bronkhorstspruit
-    listArray[15] = new Node{31.4, nullptr}; //(15,12)
+    listArray[15] = new Node{12, 31.4, nullptr}; //(15,12)
     NodePtr head16 = listArray[15];
 
-    add_node_to_list(head16, 19.6); //(15,13)
-    add_node_to_list(head16, 68.2); //(15,18)
-    add_node_to_list(head16, 70.4); //(15,22)
-    add_node_to_list(head16, 68.7); //(15,23)
+    add_node_to_list(head16, 13, 19.6); //(15,13)
+    add_node_to_list(head16, 18, 68.2); //(15,18)
+    add_node_to_list(head16, 22, 70.4); //(15,22)
+    add_node_to_list(head16, 23, 68.7); //(15,23)
 
     //Midrand
-    listArray[16] = new Node{26.2, nullptr}; //(16,14)
+    listArray[16] = new Node{14, 26.2, nullptr}; //(16,14)
     NodePtr head17 = listArray[16];
 
-    add_node_to_list(head17, 21.5); //(16,17)
-    add_node_to_list(head17, 15.6); //(16,20)
+    add_node_to_list(head17, 17, 21.5); //(16,17)
+    add_node_to_list(head17, 20, 15.6); //(16,20)
 
     //Tembisa
-    listArray[17] = new Node{57.7, nullptr}; //(17,12)
+    listArray[17] = new Node{12, 57.7, nullptr}; //(17,12)
     NodePtr head18 = listArray[17];
 
-    add_node_to_list(head18, 40.5); //(17,14)
-    add_node_to_list(head18, 21.5); //(17,16)
-    add_node_to_list(head18, 12.4); //(17,18)
-    add_node_to_list(head18, 25.4); //(17,20)
+    add_node_to_list(head18, 14, 40.5); //(17,14)
+    add_node_to_list(head18, 16, 21.5); //(17,16)
+    add_node_to_list(head18, 18, 12.4); //(17,18)
+    add_node_to_list(head18, 20, 25.4); //(17,20)
 
     //Kempton Park
-    listArray[18] = new Node{60, nullptr}; //(18,12)
+    listArray[18] = new Node{12, 60, nullptr}; //(18,12)
     NodePtr head19 = listArray[18];
 
-    add_node_to_list(head19, 68.2); //(18,15)
-    add_node_to_list(head19, 12.4); //(18,17)
-    add_node_to_list(head19, 19.3); //(18,20)
-    add_node_to_list(head19, 29); //(18,22)
-    add_node_to_list(head19, 39.2); //(18,25)
+    add_node_to_list(head19, 15, 68.2); //(18,15)
+    add_node_to_list(head19, 17, 12.4); //(18,17)
+    add_node_to_list(head19, 20, 19.3); //(18,20)
+    add_node_to_list(head19, 22, 29); //(18,22)
+    add_node_to_list(head19, 25, 39.2); //(18,25)
 
     //Randburg
-    listArray[19] = new Node{52.5, nullptr}; //(19,14)
+    listArray[19] = new Node{14, 52.5, nullptr}; //(19,14)
     NodePtr head20 = listArray[19];
 
-    add_node_to_list(head20, 15.7); //(19,20)
-    add_node_to_list(head20, 12.6); //(19,21)
+    add_node_to_list(head20, 20, 15.7); //(19,20)
+    add_node_to_list(head20, 21, 12.6); //(19,21)
 
     //Sandton
-    listArray[20] = new Node{15.6, nullptr}; //(20,16)
+    listArray[20] = new Node{16, 15.6, nullptr}; //(20,16)
     NodePtr head21 = listArray[20];
 
-    add_node_to_list(head21, 25.4); //(20,17)
-    add_node_to_list(head21, 19.3); //(20,18)
-    add_node_to_list(head21, 15.7); //(20,19)
-    add_node_to_list(head21, 36.8); //(20,25)
+    add_node_to_list(head21, 17, 25.4); //(20,17)
+    add_node_to_list(head21, 18, 19.3); //(20,18)
+    add_node_to_list(head21, 19, 15.7); //(20,19)
+    add_node_to_list(head21, 25, 36.8); //(20,25)
 
     //Roodepoort
-    listArray[21] = new Node{12.6, nullptr}; //(21,19)
+    listArray[21] = new Node{19, 12.6, nullptr}; //(21,19)
     NodePtr head22 = listArray[21];
 
-    add_node_to_list(head22, 16.2); //(21,22)
-    add_node_to_list(head22, 38.2); //(21,26)
-    add_node_to_list(head22, 15.9); //(21,27)
+    add_node_to_list(head22, 22, 16.2); //(21,22)
+    add_node_to_list(head22, 26, 38.2); //(21,26)
+    add_node_to_list(head22, 27, 15.9); //(21,27)
 
     //Krugersdorp
-    listArray[22] = new Node{62.6, nullptr}; //(22,9)
+    listArray[22] = new Node{9, 62.6, nullptr}; //(22,9)
     NodePtr head23 = listArray[22];
 
-    add_node_to_list(head23, 48.3); //(22,14)
-    add_node_to_list(head23, 16.2); //(22,21)
-    add_node_to_list(head23, 14.5); //(22,28)
+    add_node_to_list(head23, 14, 48.3); //(22,14)
+    add_node_to_list(head23, 21, 16.2); //(22,21)
+    add_node_to_list(head23, 28, 14.5); //(22,28)
 
     //Benoni
-    listArray[23] = new Node{53.9, nullptr}; //(23,12)
+    listArray[23] = new Node{12, 53.9, nullptr}; //(23,12)
     NodePtr head24 = listArray[23];
 
-    add_node_to_list(head24, 70.4); //(23,15)
-    add_node_to_list(head24, 29); //(23,18)
-    add_node_to_list(head24, 19.7); //(23,24)
-    add_node_to_list(head24, 24.8); //(23,25)
+    add_node_to_list(head24, 15, 70.4); //(23,15)
+    add_node_to_list(head24, 18, 29); //(23,18)
+    add_node_to_list(head24, 24, 19.7); //(23,24)
+    add_node_to_list(head24, 25, 24.8); //(23,25)
 
     //Springs
-    listArray[24] = new Node{69.2, nullptr}; //(24,12)
+    listArray[24] = new Node{12, 69.2, nullptr}; //(24,12)
     NodePtr head25 = listArray[24];
 
-    add_node_to_list(head25, 68.7); //(24,15)
-    add_node_to_list(head25, 19.7); //(24,23)
-    add_node_to_list(head25, 21.2); //(24,25)
-    add_node_to_list(head25, 90.7); //(24,31)
+    add_node_to_list(head25, 15, 68.7); //(24,15)
+    add_node_to_list(head25, 23, 19.7); //(24,23)
+    add_node_to_list(head25, 25, 21.2); //(24,25)
+    add_node_to_list(head25, 31, 90.7); //(24,31)
 
     //Boksburg
-    listArray[25] = new Node{24.8, nullptr}; //(25,23)
+    listArray[25] = new Node{23, 24.8, nullptr}; //(25,23)
     NodePtr head26 = listArray[25];
 
-    add_node_to_list(head26, 21.2); //(25,24)
-    add_node_to_list(head26, 22.6); //(25,26)
+    add_node_to_list(head26, 24, 21.2); //(25,24)
+    add_node_to_list(head26, 26, 22.6); //(25,26)
 
     //Alberton
-    listArray[26] = new Node{39.2, nullptr}; //(26,18)
+    listArray[26] = new Node{18, 39.2, nullptr}; //(26,18)
     NodePtr head27 = listArray[26];
 
-    add_node_to_list(head27, 36.8); //(26,20)
-    add_node_to_list(head27, 38.2); //(26,21)
-    add_node_to_list(head27, 22.6); //(26,25)
-    add_node_to_list(head27, 81.9); //(26,30)
-    add_node_to_list(head27, 45.4); //(26,31)
+    add_node_to_list(head27, 20, 36.8); //(26,20)
+    add_node_to_list(head27, 21, 38.2); //(26,21)
+    add_node_to_list(head27, 25, 22.6); //(26,25)
+    add_node_to_list(head27, 26, 81.9); //(26,30)
+    add_node_to_list(head27, 31, 45.4); //(26,31)
 
     //Soweto
-    listArray[27] = new Node{15.9, nullptr}; //(27,21)
+    listArray[27] = new Node{21, 15.9, nullptr}; //(27,21)
     NodePtr head28 = listArray[27];
 
-    add_node_to_list(head28, 38.6); //(27,26)
-    add_node_to_list(head28, 24.2); //(27,28)
-    add_node_to_list(head28, 53.1); //(27,29)
-    add_node_to_list(head28, 53.7); //(27,30)
+    add_node_to_list(head28, 26, 38.6); //(27,26)
+    add_node_to_list(head28, 28, 24.2); //(27,28)
+    add_node_to_list(head28, 29, 53.1); //(27,29)
+    add_node_to_list(head28, 30, 53.7); //(27,30)
 
     //Randfontein
-    listArray[28] = new Node{14.5, nullptr}; //(28,22)
+    listArray[28] = new Node{22, 14.5, nullptr}; //(28,22)
     NodePtr head29 = listArray[28];
 
-    add_node_to_list(head29, 24.2); //(28,27)
-    add_node_to_list(head29, 39.6); //(28,29)
+    add_node_to_list(head29, 27, 24.2); //(28,27)
+    add_node_to_list(head29, 29, 39.6); //(28,29)
 
     //Carltonville
-    listArray[29] = new Node{53.1, nullptr}; //(29,27)
+    listArray[29] = new Node{27, 53.1, nullptr}; //(29,27)
     NodePtr head30 = listArray[29];
 
-    add_node_to_list(head30, 39.6); //(29,28)
-    add_node_to_list(head30, 17.7); //(29,30)
+    add_node_to_list(head30, 28, 39.6); //(29,28)
+    add_node_to_list(head30, 30, 17.7); //(29,30)
 
     //Fochville
-    listArray[30] = new Node{81.9, nullptr}; //(30,26)
+    listArray[30] = new Node{26, 81.9, nullptr}; //(30,26)
     NodePtr head31 = listArray[30];
 
-    add_node_to_list(head31, 53.7); //(30,27)
-    add_node_to_list(head31, 17.7); //(30,29)
-    add_node_to_list(head31, 63.4); //(30,31)
-    add_node_to_list(head31, 53.4); //(30,32)
+    add_node_to_list(head31, 27, 53.7); //(30,27)
+    add_node_to_list(head31, 29, 17.7); //(30,29)
+    add_node_to_list(head31, 31, 63.4); //(30,31)
+    add_node_to_list(head31, 32, 53.4); //(30,32)
 
     //Vereeniging
-    listArray[31] = new Node{90.7, nullptr}; //(31,24)
+    listArray[31] = new Node{24, 90.7, nullptr}; //(31,24)
     NodePtr head32 = listArray[31];
 
-    add_node_to_list(head32, 45.4); //(31,26)
-    add_node_to_list(head32, 63.4); //(31,30)
-    add_node_to_list(head32, 23); //(31,32)
+    add_node_to_list(head32, 26, 45.4); //(31,26)
+    add_node_to_list(head32, 30, 63.4); //(31,30)
+    add_node_to_list(head32, 32, 23); //(31,32)
 
     //Vanderbijlpark
-    listArray[32] = new Node{53.4, nullptr}; //(32,30)
+    listArray[32] = new Node{30, 53.4, nullptr}; //(32,30)
     NodePtr head33 = listArray[32];
 
-    add_node_to_list(head33, 23); //(32,31)
+    add_node_to_list(head33, 31, 23); //(32,31)
 
 }
